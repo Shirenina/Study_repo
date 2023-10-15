b67154d2dd7a50aee04831fc25ddf9fe96af9fdd
Kate <77696180+Shirenina@users.noreply.github.com>
2023-10-15 17:54:19 +0300

    Оптимизирован css селектор кнопки, изменено сообщение в assert

--- test_items.py
+++ test_items.py
@@ -6,4 +6,4 @@ def test_add_to_cart_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(30)
-    assert len(browser.find_elements(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")) > 0, "No 'Add to cart' button on the page"
+    assert len(browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")) > 0, "No 'Add to basket' button on the page"
