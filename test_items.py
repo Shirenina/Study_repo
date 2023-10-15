c990ac962a04a0b01656216ae972706532494450
Kate <77696180+Shirenina@users.noreply.github.com>
2023-10-15 18:06:59 +0300

    Изменения в названии тестового метода

--- test_items.py
+++ test_items.py
@@ -2,8 +2,8 @@ import time
 from selenium.webdriver.common.by import By
 
 
-def test_add_to_basket_button(browser):
+def test_find_add_to_cart_button(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     time.sleep(30)
     btn_add = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
-    assert btn_add, "No 'Add to basket' button on the page"
+    assert btn_add, "No 'Add to cart' button on the page"
