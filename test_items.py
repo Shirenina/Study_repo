a5fbe7c690d91dd215f70ce6e73a3999787936a0
Kate <77696180+Shirenina@users.noreply.github.com>
2023-10-15 18:04:01 +0300

    Изменения в assert для лучшей читаемости

--- test_items.py
+++ test_items.py
@@ -2,8 +2,8 @@ import time
 from selenium.webdriver.common.by import By
 
 
-def test_add_to_cart_button(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.get(link)
+def test_add_to_basket_button(browser):
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     time.sleep(30)
-    assert len(browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")) > 0, "No 'Add to basket' button on the page"
+    btn_add = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert btn_add, "No 'Add to basket' button on the page"
